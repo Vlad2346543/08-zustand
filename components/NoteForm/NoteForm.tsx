59202988ff4ca33cc1b3f84580ae578ee07296fe
VLADYSLAV BORODKIN <114310954+Vlad2346543@users.noreply.github.com>
2026-02-22 23:36:55 +0200

    header commit

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -4,7 +4,7 @@ import css from './NoteForm.module.css';
 import { useRouter } from 'next/navigation';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createNote } from '@/lib/api';
-import { useNoteStore } from '@/lib/noteStore';
+import { useNoteStore } from '@/lib/store/noteStore';
 
 export default function NoteForm() {
   const router = useRouter();
